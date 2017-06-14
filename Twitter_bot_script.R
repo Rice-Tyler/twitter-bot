cfbe36c5f0267f3f9ccb2d48e81c704e6d6a259f
Rice Tyler <rht6nf@virginia.edu>
2017-06-14 16:02:52 +0200

    commented out extraneous code

--- Twitter_bot_script.R
+++ Twitter_bot_script.R
@@ -23,16 +23,16 @@ Year = year(Date)
 Month = month(Date)
 Day = day(Date)
 weather <- getURL( sprintf("https://www.wunderground.com/history/airport/FACT/%s/%s/%s/DailyHistory.html?format=1", Year, Month, day))
-if(0){
-  newweather = read.csv(textConnection(weather), header = T)
-  newweather$TimeSAST = strptime(newweather$TimeSAST, "%I:%M %p")
-  myweather = rbind(myweather, newweather)
-}
+#if(0){
+ # newweather = read.csv(textConnection(weather), header = T)
+ # newweather$TimeSAST = strptime(newweather$TimeSAST, "%I:%M %p")
+#  myweather = rbind(myweather, newweather)
+#}
 myweather <- read.csv(textConnection(weather), header=T)
 head(myweather)
 avg_temp = mean(myweather$TemperatureC)
 myweather$TimeSAST = strptime(myweather$TimeSAST, "%I:%M %p")
-ggplot(myweather, aes(x = TemperatureC, fill = Conditions)) + geom_density(alpha = .5)
+#ggplot(myweather, aes(x = TemperatureC, fill = Conditions)) + geom_density(alpha = .5)
 n = nrow(myweather)
 current_temp = myweather[n, 2]
 current_cond = myweather[n, 12]
